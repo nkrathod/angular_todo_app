934e0c27e3103c83b4bbe98e04b6222ed03b0140
Nitesh Rathod <niteshrathod15205@gmail.com>
2023-08-18 01:27:58 +0530

    nav and insert todo UI added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+// Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
